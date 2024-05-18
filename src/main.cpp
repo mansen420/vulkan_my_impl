fc9a08cdc5ad6014ac613eee5187baea833ce957
Mansen <husenalasady21@gmail.com>
2024-05-19 00:23:26 +0300

    Add a ton of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,28 @@ struct vulkan_communication_instance_init_info
     const char* app_name;
 };
 
-
 /************************************************HELPER FUNCTIONS, PRIVATE STRUCTS************************************************/
 
+VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback_fun(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, 
+VkDebugUtilsMessageTypeFlagsEXT message_type, const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data, void* p_user_data)
+{
+    const char* message_severity_text;
+    switch (message_severity)
+    {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+        message_severity_text = "WARNING";
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+        message_severity_text = "ERROR";
+    default:
+        message_severity_text = "UNDETERMINED SEVERITY";
+        break;
+    }
+    ENG_ERR_LOG << "VALIDATION LAYER : " << p_callback_data->pMessage << " (SEVERITY : "<< message_severity_text << ')'
+    << std::endl;
+    return VK_FALSE;
+}
+    
 struct queue_indices
 {
     std::optional<uint32_t> graphics_queue_index, present_queue_index;
@@ -58,51 +77,108 @@ struct vk_extension_info
     std::vector<const char*> extensions;
     std::vector<const char*> layers;
 };
+std::vector<const char*> get_extension_info(VkPhysicalDevice device)
+{
+    std::vector<const char*> required_extension_names;
+
+    required_extension_names.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+
+    vk_extension_info info{};
+    info.extensions = required_extension_names;
+
+    return required_extension_names;
+}
+VkApplicationInfo get_app_info(const char* app_name)
+{
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.apiVersion = VK_API_VERSION_1_0;
+    app_info.engineVersion = VK_MAKE_VERSION(1.0, 0.0, 0.0);
+    app_info.applicationVersion = VK_MAKE_VERSION(1.0, 0.0, 0.0);
+    app_info.pApplicationName = app_name;
+    return app_info;
+}
+VkDebugUtilsMessengerCreateInfoEXT get_debug_create_info()
+{
+    VkDebugUtilsMessengerCreateInfoEXT create_info{};
+
+    create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    create_info.pfnUserCallback = debug_callback_fun;
+    create_info.pUserData = nullptr;
+
+    return create_info;
+}
+queue_indices get_queue_indices(VkPhysicalDevice phys_device)
+{
+    queue_indices result;
+
+    uint32_t property_count;
+    vkGetPhysicalDeviceQueueFamilyProperties(phys_device, &property_count, nullptr);
+    std::vector<VkQueueFamilyProperties> queue_families(property_count);
+    for(size_t i = 0; i < property_count; i++)
+    {
+        if(queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            result.graphics_queue_index = (uint32_t) i;
+
+    }
+    return result;
+}
+std::vector<VkPhysicalDevice> get_physical_devices(VkInstance instance)
+{
+    uint32_t phys_devices_count;
+    vkEnumeratePhysicalDevices(instance, &phys_devices_count, nullptr);
+    std::vector<VkPhysicalDevice> phys_devices(phys_devices_count);
+    vkEnumeratePhysicalDevices(instance,&phys_devices_count, phys_devices.data());
+    return phys_devices;
+}
+
+
+bool check_support(size_t available_name_count, const char** available_names, const char** required_names, size_t required_name_count)
+{
+    if(required_name_count == 0)
+        return true;
+    if(available_property_count == 0)
+    {
+        ENG_LOG << "WARNING : using a zero-sized array\n";
+        return false;
+    }
+    if(available_names == nullptr || required_names == nullptr)
+    {
+        ENG_ERR_LOG << "WARNING : using nullptr\n";
+        return false;
+    }
+    
+    bool result = true;
+    for(size_t i = 0; i < required_name_count; i++)
+    {
+        bool found = false;
+        for(size_t j = 0; j < available_property_count; j++)
+        {
+            if(strcmp(required_names[i], available_names[j]) == 0)
+            {
+                ENG_LOG << required_names[i] << " SUPPORTED.\n";
+                found = true;
+                break;
+            }
+        }
+        result &= found;
+        if(!found)
+            ENG_LOG << required_names[i] << " NOT SUPPORTED.\n";
+    }
+    return result;
+};
+bool check_support(std::vector<const char*> available_names, std::vector<const char*> required_names)
+{
+    return check_support(available_names.size(), available_names.data(), required_names.data(), required_names.size());
+}
+
 //Attempts to create vulkan instance in instance_ref.
 //Also checks for extension and layer support, throwing a std::runtime_error in case of failure.
 VkResult init_vk_instance(VkInstance& instance_ref, vk_extension_info ext_info, const VkApplicationInfo app_info, void* next_ptr)
 {
     //first, check for extension and layer support
-    std::function<bool(size_t available_property_count, const char** available_names, const char** required_names,
-    size_t required_name_count)> check_support;
-    check_support = [](size_t available_property_count, const char** available_names, const char** required_names,
-    size_t required_name_count) -> bool
-    {
-        if(required_name_count == 0)
-        {
-            return true;
-        }
-        if(available_property_count == 0)
-        {
-            ENG_LOG << "WARNING : using a zero-sized array\n";
-            return false;
-        }
-        if(available_names == nullptr || required_names == nullptr)
-        {
-            ENG_ERR_LOG << "WARNING : using nullptr\n";
-            return false;
-        }
-        
-        bool result = true;
-        for(size_t i = 0; i < required_name_count; i++)
-        {
-            bool found = false;
-            for(size_t j = 0; j < available_property_count; j++)
-            {
-                if(strcmp(required_names[i], available_names[j]) == 0)
-                {
-                    ENG_LOG << required_names[i] << " SUPPORTED.\n";
-                    found = true;
-                    break;
-                }
-            }
-            result &= found;
-            if(!found)
-                ENG_LOG << required_names[i] << " NOT SUPPORTED.\n";
-        }
-        return result;
-    };
-  
     uint32_t instance_property_count;
     vkEnumerateInstanceExtensionProperties(nullptr, &instance_property_count,
     nullptr);
@@ -141,36 +217,32 @@ VkResult init_vk_instance(VkInstance& instance_ref, vk_extension_info ext_info, 
     return vkCreateInstance(&instance_create_info, nullptr, &instance_ref);
 }
 
-VkApplicationInfo get_app_info(const char* app_name)
+std::vector<const char*> get_properties(VkPhysicalDevice device)
 {
-    VkApplicationInfo app_info{};
-    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    app_info.apiVersion = VK_API_VERSION_1_0;
-    app_info.engineVersion = VK_MAKE_VERSION(1.0, 0.0, 0.0);
-    app_info.applicationVersion = VK_MAKE_VERSION(1.0, 0.0, 0.0);
-    app_info.pApplicationName = app_name;
-    return app_info;
+    uint32_t count;
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &count, nullptr);
+    std::vector<VkExtensionProperties> properties(count);
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &count, properties.data());
+//HACK the above calls could fail
+    std::vector<const char*> property_names(count);
+    for (const auto& property : properties)
+        property_names.push_back(property.extensionName);
+    return property_names;
 }
-
-queue_indices get_queue_indices(VkPhysicalDevice phys_device)
+bool is_complete(queue_indices indices)
 {
-    queue_indices result;
-
-    uint32_t property_count;
-    vkGetPhysicalDeviceQueueFamilyProperties(phys_device, &property_count, nullptr);
-    std::vector<VkQueueFamilyProperties> queue_families(property_count);
-    for(size_t i = 0; i < property_count; i++)
-    {
-        if(queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
-            result.graphics_queue_index = (uint32_t) i;
-    }
-    return result;
+    return indices.graphics_queue_index.has_value() && indices.present_queue_index.has_value();
 }
 bool is_adequate(VkPhysicalDevice phys_device)
 {
     queue_indices indices = get_queue_indices(phys_device);
 
-    return indices.graphics_queue_index.has_value();
+    std::vector available_ext = get_properties(phys_device);
+    std::vector required_ext  = get_extension_info(phys_device);
+
+    bool extensions_supported = check_support(available_ext, required_ext);
+
+    return extensions_supported;
 }
 
 void destroy_debug_messenger(VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger)
@@ -192,18 +264,30 @@ public:
     virtual void destroy() = 0;
 };
 
-//parent kills children before it kills itself : generic data structure PITA
-
-template <class handle_t> class vk_object : public destroyable
-{
-public:
-    virtual void destroy() {};
-    handle_t handle = VK_NULL_HANDLE;
-};
-
-class parent
+template <class handle_t> class vk_object : virtual public destroyable
 {
 protected:
+    virtual void free_obj(){};
+public:
+    handle_t handle = VK_NULL_HANDLE;
+    virtual void destroy() override final 
+    {
+        if(handle == VK_NULL_HANDLE)
+            return;
+        free_obj();
+        handle = VK_NULL_HANDLE;
+    };
+    virtual ~vk_object(){destroy();}
+};
+
+class parent : virtual public destroyable
+{
+protected:
+    virtual void destroy_children() final
+    {
+        for(auto& child : children)
+            child->destroy();
+    }
     std::vector<destroyable*> children;
 public:
     void add_child(destroyable* child)
@@ -212,10 +296,9 @@ public:
     }
 };
 template <class parent_t, std::enable_if_t<std::is_base_of<parent, parent_t>::value && !std::is_same<parent_t, parent>::value, int> = 0 >
-class child : public destroyable
+class child : virtual public destroyable
 {
 public:
-    virtual void destroy() = 0;
     child(parent_t* parent_ptr) 
     {
         this->parent_ptr = parent_ptr;
@@ -228,27 +311,27 @@ public:
 class vk_instance        : public vk_object<VkInstance>              , public parent
 {
 public:
-    virtual void destroy() override final
+    virtual void free_obj() override final
     {
-        for(auto& child : children)
-            child->destroy();
-        vkDestroyInstance(this->handle, nullptr);
+        destroy_children();
+        vkDestroyInstance(handle, nullptr);
     }
 };
 class vk_debug_messenger : public vk_object<VkDebugUtilsMessengerEXT>, public child<vk_instance>
 {
 public:
-    vk_instance* parent;
     vk_debug_messenger(vk_instance* parent) : child<vk_instance>(parent) {}
-    void destroy() override final
-    {
-        destroy_debug_messenger(parent->handle, this->handle);
-    }
+    virtual void free_obj() override final{destroy_debug_messenger(parent_ptr->handle, this->handle);}
 };
-class vk_phys_device     : public vk_object<VkPhysicalDevice>        , public child<vk_instance>
+class vk_surface         : public vk_object<VkSurfaceKHR>            , public child<vk_instance>
 {
-    vk_phys_device(vk_instance* parent) : child<vk_instance>(parent) {}
+public:
+    vk_surface(vk_instance* parent) : child<vk_instance>(parent) {}
+    virtual void free_obj(){vkDestroySurfaceKHR(parent_ptr->handle, this->handle, nullptr);}
 };
+typedef vk_object<VkPhysicalDevice> vk_phys_device;
+
+
 //only call the vulkan API here
 class vulkan_communication_instance
 {
@@ -262,6 +345,11 @@ public:
         if(DEBUG_MODE)
             init_validation_layer();
         init_phys_device();
+
+        static vk_surface surface_obj(&INSTANCE_obj);
+        if (GLFW_INTERFACE.init_vk_surface(INSTANCE, surface_obj.handle) != VK_SUCCESS)
+            throw std::runtime_error("Failed to initialize vulkan window surface.");
+        DESTROY_QUEUE.push_back(&surface_obj);
     }
     //run this inside the render loop
     void invoke()
@@ -272,13 +360,14 @@ public:
     //note that a vulkan_communication_layer object can not be restarted after termination
     void terminate()
     {
-        for(auto& object : DESTROY_QUEUE)
-            object.destroy();
+        const size_t SIZE = DESTROY_QUEUE.size();
+        for(size_t i = 0; i < SIZE; i++)
+            DESTROY_QUEUE[SIZE-i-1]->destroy();
 
         GLFW_INTERFACE.terminate();
     }
 private:
-    vk_extension_info get_extension_info()
+    static vk_extension_info get_extension_info(VkInstance instance)
     {
         const bool& ENABLE_VALIDATION_LAYERS = DEBUG_MODE;
 
@@ -308,40 +397,24 @@ private:
         if(!DEBUG_MODE)
             ext_ptr = nullptr;
 
-        init_vk_instance(INSTANCE, get_extension_info(), get_app_info(app_name), ext_ptr);
+        init_vk_instance(INSTANCE, get_extension_info(INSTANCE), get_app_info(app_name), ext_ptr);
+        DESTROY_QUEUE.push_back(&INSTANCE_obj);
     }
     void init_phys_device()
     {
-        uint32_t phys_devices_count;
-        vkEnumeratePhysicalDevices(INSTANCE, &phys_devices_count, nullptr);
-        std::vector<VkPhysicalDevice> phys_devices(phys_devices_count);
-        vkEnumeratePhysicalDevices(INSTANCE,&phys_devices_count, phys_devices.data());
+        const auto phys_devices = get_physical_devices(INSTANCE);
 
+        std::vector<VkPhysicalDevice> candidates;
         for( const auto& device : phys_devices)
-        {
             if(is_adequate(device))
-            {
-                PHYS_DEVICE = device;
-                break;
-            }
-        }
-    } 
-    
-    VkDebugUtilsMessengerCreateInfoEXT get_debug_create_info()
-    {
-        VkDebugUtilsMessengerCreateInfoEXT create_info{};
+                candidates.push_back(device);
 
-        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-        create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-        create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-        create_info.pfnUserCallback = debug_callback_fun;
-        create_info.pUserData = nullptr;
-
-        return create_info;
+        DESTROY_QUEUE.push_back(&PHYS_DEVICE_obj);
     }
+    
     void init_validation_layer()
     {
-        static VkDebugUtilsMessengerEXT debug_messenger;
+        static vk_debug_messenger debug_messenger(&INSTANCE_obj);
 
         auto create_info = get_debug_create_info();
 
@@ -349,29 +422,12 @@ private:
         INSTANCE, "vkCreateDebugUtilsMessengerEXT");
         if(fun == nullptr)
             throw std::runtime_error("Failed to get function pointer : \"vkCreateDebugUtilsMessengerEXT.\"");
-        if(fun(INSTANCE, &create_info, nullptr, &debug_messenger) != VK_SUCCESS)
+        if(fun(INSTANCE, &create_info, nullptr, &debug_messenger.handle) != VK_SUCCESS)
             throw std::runtime_error("Failed to create debug util messenger object.");
+
+        DESTROY_QUEUE.push_back(&debug_messenger);   
     }
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback_fun(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, 
-    VkDebugUtilsMessageTypeFlagsEXT message_type, const VkDebugUtilsMessengerCallbackDataEXT* p_callback_data, void* p_user_data)
-    {
-        const char* message_severity_text;
-        switch (message_severity)
-        {
-        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
-            message_severity_text = "WARNING";
-            break;
-        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
-            message_severity_text = "ERROR";
-        default:
-            message_severity_text = "UNDETERMINED SEVERITY";
-            break;
-        }
-        ENG_ERR_LOG << "VALIDATION LAYER : " << p_callback_data->pMessage << " (SEVERITY : "<< message_severity_text << ')'
-        << std::endl;
-        return VK_FALSE;
-    }
-    
+
     //only call GLFW functions here
     class GLFW_window_interface
     {
@@ -398,15 +454,22 @@ private:
             std::vector<const char*> extensions(data, data + count);
             return extensions;
         }
+        VkResult init_vk_surface(VkInstance instance, VkSurfaceKHR& surface)
+        {
+            return glfwCreateWindowSurface(instance, WINDOW_PTR, nullptr, &surface);
+        }
     private:
         GLFWwindow* WINDOW_PTR;
     };
     GLFW_window_interface GLFW_INTERFACE;
 
-    VkInstance                      INSTANCE;
-    VkPhysicalDevice             PHYS_DEVICE;
+    vk_instance                   INSTANCE_obj;
+    vk_phys_device             PHYS_DEVICE_obj;
 
-    std::vector<destroyable>   DESTROY_QUEUE;
+    VkInstance&       INSTANCE    =    INSTANCE_obj.handle;
+    VkPhysicalDevice& PHYS_DEVICE = PHYS_DEVICE_obj.handle;
+
+    std::vector<destroyable*>   DESTROY_QUEUE;
 };
 
 
