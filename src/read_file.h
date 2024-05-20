a499b929d8198369358437e58848563d89a7283d
Mansen <husenalasady21@gmail.com>
2024-05-20 22:21:34 +0300

    Update read file function

--- src/read_file.h
+++ src/read_file.h
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <sstream>
 #include <cstring>
-
+#include <vector>
+//Reads binary file into C-style string
 //Caller must ensure that file_contents_holder is delete[]`d!
 inline bool read_binary_file(const char* file_path, char* &file_contents_holder, size_t& file_size)
 {
@@ -23,4 +24,39 @@ inline bool read_binary_file(const char* file_path, char* &file_contents_holder,
     file_reader.close();
 
     return true;
+}
+//Reads binary file into contents
+//Be warned that previous contents of the vector will be lost
+inline bool read_binary_file(const char* file_path, std::vector<char>& contents)
+{
+    std::ifstream file_reader(file_path, std::ios::ate | std::ios::binary);
+
+    if(!file_reader.is_open())
+        return false;
+
+    //get file size then seek back to beginning
+    size_t file_size = (size_t)file_reader.tellg();
+    file_reader.seekg(0);
+    
+    contents.resize(file_size);
+    //read file contents
+    file_reader.read(contents.data(), file_size);
+
+    file_reader.close();
+
+    return true;
+}
+//Will read first match from search_paths
+inline bool read_binary_file(std::vector<const char*> search_paths, const char* filename, std::vector<char>& contents)
+{
+    bool result;
+    for(const auto& directory : search_paths)
+    {
+        std::string path(directory);
+        path.append(filename);
+        result = read_binary_file(path.c_str(), contents);
+        if (result)
+            break;
+    }
+    return result;
 }
